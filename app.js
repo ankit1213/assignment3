c025a9b37904e7d7f34d3d9ffe6f8d6133377666
Ankit Saxena <juetankit@gmail.com>
2022-11-15 23:04:16 +0530

    App.js Added

---
+++ app.js
@@ -0,0 +1,6 @@
+const { myFileWriter, myFileUpdater, myFileReader, myFileDeleter }=require("./index.js")
+
+//  myFileWriter("content.txt","We are in the peer review session")
+//  myFileUpdater("content.txt","This is an Updated Data")
+// myFileReader("content.txt")
+myFileDeleter("content.txt")
